c7a365ebbc4af1b2f46fd4c26c9267be46da59c4
Zaahy <zaahy10@gmail..com>
2026-03-01 00:17:47 -0500

    added feedback form

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -11,7 +11,21 @@ const FeedbackForm = () => {
       <form className="feedback-form">
         <h2>We'd Love to Hear From You!</h2>
         <p>Please share your feedback with us.</p>
-        
+        <input
+          type="text"
+          name="name"
+          placeholder="Your Name"
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Your Email"
+        />
+        <textarea
+          name="feedback"
+          placeholder="Your Feedback"
+        ></textarea>
+        <button type="submit">Submit Feedback</button>
       </form>
     </>
   );
